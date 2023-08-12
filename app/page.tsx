d2a6fbd1724891f2962748c8710be2cfad3b8926
Ariiq354 <realgoldcraft@gmail.com>
2023-08-12 18:02:22 +0700

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Pelayanan from "@/components/landing/Pelayanan";
 import TentangKami from "@/components/landing/TentangKami";
-import Hero from "@/components/landing/hero";
-import Statistik from "@/components/landing/statistik";
+import Hero from "@/components/landing/Hero";
+import Statistik from "@/components/landing/Statistik";
 import Artikel from "@/components/landing/Artikel";
 
 export default function Home() {
