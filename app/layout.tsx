3113a1e4f5707e514fff53385381e145db5131a6
Ariiq354 <realgoldcraft@gmail.com>
2023-09-10 10:58:15 +0700

    change2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
-"use client";
-
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import Navbar from "@/components/template/Navbar";
 import Footer from "@/components/template/Footer";
-import Transition from "@/components/transition";
-
-import { usePathname } from "next/navigation";
-
-import { AnimatePresence, motion } from "framer-motion";
-import path from "path";
 
 const roboto = Roboto({
   weight: "400",
@@ -27,18 +19,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
   return (
     <html>
       <body className={roboto.className}>
         <Navbar />
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.div key={pathname} className="h-full">
-            <Transition />
-            {children}
-          </motion.div>
-        </AnimatePresence>
+        {children}
         <Footer />
       </body>
     </html>
