041a8b5b3b5d47c0266c75c165d5aa77ac550bef
Ariiq354 <95619165+Ariiq354@users.noreply.github.com>
2023-09-12 15:07:45 +0700

    Update Statistik.tsx

--- components/landing/Statistik.tsx
+++ components/landing/Statistik.tsx
@@ -5,13 +5,8 @@ import { FaBedPulse } from "react-icons/fa6";
 
 export default function Statistik() {
   return (
-<<<<<<< HEAD
     <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 container justify-center gap-6 z-10">
       <div className="flex flex-col gap-2 border-4 border-emerald-600 items-center bg-white shadow-md py-4 px-12 text-emerald-700">
-=======
-    <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 container justify-center gap-6">
-      <div className="flex flex-col gap-2 border-4 border-emerald-600 items-center bg-white shadow-md py-4 px-12 text-emerald-700 rounded-lg">
->>>>>>> bc52eeb103b30de218007466bf37aefa156f8367
         <FaUserMd size={60} />
         <div className="font-extrabold text-6xl text drop-shadow-md mt-4">
           120+
